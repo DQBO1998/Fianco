ff661c1eafe1b94ffe47b3323f09c661920b03b8
Dennis A. Mertens <da.mertens.velasquez@proton.me>
2024-09-27 13:46:40 +0200

    Memory allocation only happens when entering a node - once. Lateral exploration now overrides instead of instantiate.

--- auto.py
+++ auto.py
@@ -10,10 +10,8 @@ import numba as nb # type: ignore
 
 
 @nb.njit # type: ignore
-def capts(ply: int, brd: Mat) -> tuple[int, NDArray[number]]:
+def capts(ply: int, brd: Mat, frto: NDArray[number]) -> int:
     at = 0
-    top = np.sum(brd[ply]) * 2
-    out = np.empty((top, 2, 2), dtype=number)
     dy = vdir(ply)
     for y in range(0, brd.shape[1]):
         for x in range(0, brd.shape[2]):
@@ -23,39 +21,37 @@ def capts(ply: int, brd: Mat) -> tuple[int, NDArray[number]]:
                         and 0 <= y + 2 * dy < brd.shape[1] and 0 <= x + 2 * dx < brd.shape[2] \
                         and brd[1 - ply, y + dy, x + dx] \
                         and ~(brd[1 - ply, y + 2 * dy, x + 2 * dx] | brd[ply, y + 2 * dy, x + 2 * dx]):
-                        out[at, 0, 0] = y
-                        out[at, 0, 1] = x
-                        out[at, 1, 0] = y + 2 * dy
-                        out[at, 1, 1] = x + 2 * dx
+                        frto[at, 0, 0] = y
+                        frto[at, 0, 1] = x
+                        frto[at, 1, 0] = y + 2 * dy
+                        frto[at, 1, 1] = x + 2 * dx
                         at += 1
-    return at, out
+    return at
 
 
 @nb.njit # type: ignore
-def steps(ply: int, brd: Mat) -> tuple[int, NDArray[number]]:
+def steps(ply: int, brd: Mat, frto: NDArray[number]) -> int:
     at = 0
-    top = np.sum(brd[ply]) * 3
-    out = np.empty((top, 2, 2), dtype=number)
     for y in range(0, brd.shape[1]):
         for x in range(0, brd.shape[2]):
             if brd[ply, y, x]:
                 for dy, dx in ((vdir(ply), 0), (0, -1), (0, +1)):
                     if 0 <= y + dy < brd.shape[1] and 0 <= x + dx < brd.shape[2] \
                         and not brd[ply, y + dy, x + dx] and not brd[1 - ply, y + dy, x + dx]:
-                            out[at, 0, 0] = y
-                            out[at, 0, 1] = x
-                            out[at, 1, 0] = y + dy
-                            out[at, 1, 1] = x + dx
+                            frto[at, 0, 0] = y
+                            frto[at, 0, 1] = x
+                            frto[at, 1, 0] = y + dy
+                            frto[at, 1, 1] = x + dx
                             at += 1
-    return at, out
+    return at
 
 
 @nb.njit # type: ignore
-def all_moves(ply: int, brd: Mat) -> tuple[int, NDArray[number]]:
-    cnt, frto = capts(ply, brd)
+def all_moves(ply: int, brd: Mat, frto: NDArray[number]) -> int:
+    cnt = capts(ply, brd, frto)
     if cnt <= 0:
-        cnt, frto = steps(ply, brd)
-    return cnt, frto
+        cnt = steps(ply, brd, frto)
+    return cnt
 
 
 @nb.njit # type: ignore
@@ -73,28 +69,31 @@ def scr_at(wrt: int, end: Mat, brd: Mat) -> float:
 
 
 @nb.njit # type: ignore
-def nxtst(ply: int, fr: YX, to: YX, brd: Mat) -> Mat:
-    brd = np.copy(brd)
+def nxtst(ply: int, fr: YX, to: YX, brd: Mat) -> None:
     if is_capt(ply, fr, to, brd):
         th = (fr + to) // 2
         brd[1 - ply, th[0], th[1]] = False
     brd[ply, fr[0], fr[1]] = False
     brd[ply, to[0], to[1]] = True
-    return brd
 
 
 @nb.njit # type: ignore
 def αβ_search(wrt: int, 
               ply: int, dpth: int, α: float, β: float, 
               end: Mat, brd: Mat, 
-              _ndc: NDArray[np.uint64]) -> float:
+              meta: NDArray[np.uint64]) -> float:
     if dpth <= 0 or is_end(end, brd):
         return scr_at(wrt, end, brd)
     scr = -inf
-    cnt, frto = all_moves(ply, brd)
+    max = np.sum(brd[ply]) * 5
+    frto = np.empty((max, 2, 2), dtype=number)
+    cnt = all_moves(ply, brd, frto)
+    nxt_brd = np.empty(brd.shape, dtype=brd.dtype)
     for i in range(cnt):
-        _ndc[0] += 1
-        val = -αβ_search(wrt, 1 - ply, dpth - 1, -β, -α, end, nxtst(ply, frto[i, 0], frto[i, 1], brd), _ndc)
+        meta[0] += 1
+        nxt_brd[:, :, :] = brd
+        nxtst(ply, frto[i, 0], frto[i, 1], nxt_brd)
+        val = -αβ_search(wrt, 1 - ply, dpth - 1, -β, -α, end, nxt_brd, meta)
         if val > scr:
             scr = val
         if scr > α:
@@ -105,20 +104,25 @@ def αβ_search(wrt: int,
 
 
 def think(lst: Engine, dpth: int = 3, αβ: tuple[float, float] = (-inf, +inf)) -> tuple[int, tuple[YX, YX]]:
-    ndc = np.zeros((1,), np.uint64)
+    meta = np.zeros((1,), np.uint64)
     lst = deepcopy(lst)
     α, β = αβ
     out: tuple[YX, YX] | None = None
     scr = -inf
-    cnt, frto = all_moves(lst.ply, lst.brd)
+    max = np.sum(lst.brd[lst.ply]) * 5
+    frto = np.empty((max, 2, 2), dtype=number)
+    cnt = all_moves(lst.ply, lst.brd, frto)
+    nxt_brd = np.empty(lst.brd.shape, dtype=lst.brd.dtype)
     for i in range(cnt):
-        ndc[0] += 1
-        val = -αβ_search(lst.ply, 1 - lst.ply, dpth - 1, -β, -α, lst.end, nxtst(lst.ply, frto[i, 0], frto[i, 1], lst.brd), ndc)
+        meta[0] += 1
+        nxt_brd[:, :, :] = lst.brd
+        nxtst(lst.ply, frto[i, 0], frto[i, 1], nxt_brd)
+        val = -αβ_search(lst.ply, 1 - lst.ply, dpth - 1, -β, -α, lst.end, nxt_brd, meta)
         if val > scr:
             scr = val
             out = (frto[i, 0], frto[i, 1])
     assert out is not None
-    return ndc[0], out
+    return meta[0], out
 
 
 def monke(lst: Engine, rnd: Generator = default_rng(42)) -> tuple[int, tuple[YX, YX]]:
