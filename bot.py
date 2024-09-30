609aab95a5bc11c869b2bb4e2a014b632835f170
Dennis A. Mertens <da.mertens.velasquez@proton.me>
2024-09-30 18:21:10 +0200

    Lots of things changed...

--- bot.py
+++ bot.py
@@ -1,26 +1,42 @@
 
-from math import inf
+from collections.abc import Callable
 from copy import deepcopy
+from typing import Any, cast
 from fianco import number, Mat, YX, is_capt, win, vdir, Engine
 from numpy.typing import NDArray
 from numpy.random import default_rng, Generator
+from numpy import inf
 
 import numpy as np
 import numba as nb # type: ignore
 
 
+M: int = 1000000
+
+
+def dbg(func: Callable[..., Any]) -> Callable[..., Any]:
+    def wrap(*args: Any, **kwargs: Any) -> Any:
+        org = set(func.signatures) # type: ignore
+        ret = func(*args, **kwargs)
+        new = set(func.signatures) # type: ignore
+        if new != org:
+            print(f'compiled `{func.__name__}` for `{new}`')
+        return ret
+    return wrap
+
+
 @nb.njit # type: ignore
-def capts(ply: int, brd: Mat, frto: NDArray[number]) -> int:
+def capts(wrt: int, brd: Mat, frto: NDArray[number]) -> int:
     at = 0
-    dy = vdir(ply)
+    dy = vdir(wrt)
     for y in range(0, brd.shape[1]):
         for x in range(0, brd.shape[2]):
-            if brd[ply, y, x]:
+            if brd[wrt, y, x]:
                 for dx in (-1, +1):
                     if 0 <= y + dy < brd.shape[1] and 0 <= x + dx < brd.shape[2] \
                         and 0 <= y + 2 * dy < brd.shape[1] and 0 <= x + 2 * dx < brd.shape[2] \
-                        and brd[1 - ply, y + dy, x + dx] \
-                        and not (brd[1 - ply, y + 2 * dy, x + 2 * dx] | brd[ply, y + 2 * dy, x + 2 * dx]):
+                        and brd[1 - wrt, y + dy, x + dx] \
+                        and not (brd[1 - wrt, y + 2 * dy, x + 2 * dx] | brd[wrt, y + 2 * dy, x + 2 * dx]):
                         frto[at, 0, 0] = y
                         frto[at, 0, 1] = x
                         frto[at, 1, 0] = y + 2 * dy
@@ -30,14 +46,14 @@ def capts(ply: int, brd: Mat, frto: NDArray[number]) -> int:
 
 
 @nb.njit # type: ignore
-def steps(ply: int, brd: Mat, frto: NDArray[number]) -> int:
+def steps(wrt: int, brd: Mat, frto: NDArray[number]) -> int:
     at = 0
     for y in range(0, brd.shape[1]):
         for x in range(0, brd.shape[2]):
-            if brd[ply, y, x]:
-                for dy, dx in ((vdir(ply), 0), (0, -1), (0, +1)):
+            if brd[wrt, y, x]:
+                for dy, dx in ((vdir(wrt), 0), (0, -1), (0, +1)):
                     if 0 <= y + dy < brd.shape[1] and 0 <= x + dx < brd.shape[2] \
-                        and not brd[ply, y + dy, x + dx] and not brd[1 - ply, y + dy, x + dx]:
+                        and not brd[wrt, y + dy, x + dx] and not brd[1 - wrt, y + dy, x + dx]:
                             frto[at, 0, 0] = y
                             frto[at, 0, 1] = x
                             frto[at, 1, 0] = y + dy
@@ -47,12 +63,17 @@ def steps(ply: int, brd: Mat, frto: NDArray[number]) -> int:
 
 
 @nb.njit # type: ignore
-def all_moves(ply: int, brd: Mat) -> tuple[int, NDArray[number]]:
-    frto = np.empty((np.sum(brd[ply]) * 3, 2, 2), dtype=number)
-    cnt = capts(ply, brd, frto)
+def all_moves(wrt: int, brd: Mat) -> tuple[int, NDArray[number]]:
+    frto = np.empty((np.sum(brd[wrt]) * 3, 2, 2), dtype=number)
+    cnt = capts(wrt, brd, frto)
     if cnt <= 0:
-        cnt = steps(ply, brd, frto)
+        cnt = steps(wrt, brd, frto)
     return cnt, frto
+
+
+@nb.njit # type: ignore
+def is_qui(wrt: int, brd: Mat) -> bool:
+    return False
 
 
 @nb.njit # type: ignore
@@ -62,39 +83,43 @@ def is_end(end: Mat, brd: Mat) -> bool | np.bool_:
 
 @nb.njit # type: ignore
 def scr_at(wrt: int, end: Mat, brd: Mat) -> float:
-    w0 = win(0, end, brd)
-    if w0 or win(1, end, brd):
-        top = 0 if w0 else 1
-        return -((top == wrt) - (top != wrt))
-    return -((brd[wrt].sum() - brd[1 - wrt].sum()) / 15.)
+    for p in (0, 1):
+        if win(p, end, brd):
+            if p == wrt:
+                return M
+            return -M
+    s = np.sum(brd[wrt])
+    o = np.sum(brd[1 - wrt])
+    Δ = s - o
+    λ = 0.6
+    return λ * Δ + (1 - λ) * 0
 
 
 @nb.njit # type: ignore
-def make(capt: bool, flip: bool, ply: int, fr: YX, to: YX, brd: Mat) -> None:
+def make(capt: np.bool_, flip: bool, wrt: int, fr: YX, to: YX, brd: Mat) -> None:
     if capt:
         th = (fr + to) // 2
-        brd[1 - ply, th[0], th[1]] = flip
-    brd[ply, fr[0], fr[1]] = flip
-    brd[ply, to[0], to[1]] = not flip
+        brd[1 - wrt, th[0], th[1]] = flip
+    brd[wrt, fr[0], fr[1]] = flip
+    brd[wrt, to[0], to[1]] = not flip
 
 
 @nb.njit # type: ignore
 def search(root: bool,
-           wrt: int, 
-           ply: int, dpth: int, α: float, β: float, 
+           wrt: int, dpth: int, α: float, β: float, 
            end: Mat, brd: Mat, 
            ndc: NDArray[np.uint64], out: NDArray[number]) -> float:
     if dpth <= 0 or is_end(end, brd):
         return scr_at(wrt, end, brd)
     scr = -inf
-    cnt, frto = all_moves(ply, brd)
+    cnt, frto = all_moves(wrt, brd)
     for i in range(cnt):
         ndc[0] += 1
         fr = frto[i, 0]; to = frto[i, 1]
-        capt = is_capt(ply, fr, to, brd)
-        make(capt, False, ply, fr, to, brd)
-        val = -search(False, wrt, 1 - ply, dpth - 1, -β, -max(α, scr), end, brd, ndc, out)
-        make(capt, True, ply, fr, to, brd)
+        capt = is_capt(wrt, fr, to, brd)
+        make(capt, False, wrt, fr, to, brd)
+        val = -search(False, 1 - wrt, dpth - 1, -β, -max(α, scr), end, brd, ndc, out)
+        make(capt, True, wrt, fr, to, brd)
         if val > scr:
             scr = val
             if root:
@@ -105,16 +130,25 @@ def search(root: bool,
     return scr
 
 
+@dbg
+@nb.njit # type: ignore
+def root(wrt: int, dpth: int, α: float, β: float, 
+         end: Mat, brd: Mat, 
+         ndc: NDArray[np.uint64], out: NDArray[number]) -> float:
+    return search(True, wrt, dpth, -inf, +inf, end, brd, ndc, out)
+    
+
+
 def think(lst: Engine, dpth: int = 3) -> tuple[int, float, tuple[YX, YX]]:
     lst = deepcopy(lst)
     ndc = np.zeros((1,), np.uint64)
     out = np.zeros((2, 2), dtype=number)
-    exp = search(True, lst.ply, lst.ply, dpth, -inf, +inf, lst.end, lst.brd, ndc, out)
-    return ndc[0], exp, out
+    exp = root(lst.wrt, dpth, -inf, +inf, lst.end, lst.brd, ndc, out)
+    return ndc[0], exp, cast(tuple[YX, YX], out)
 
 
 def monke(lst: Engine, rnd: Generator = default_rng(42)) -> tuple[int, int, tuple[YX, YX]]:
-    cnt, frto = all_moves(lst.ply, lst.brd)
+    cnt, frto = all_moves(lst.wrt, lst.brd)
     idx = rnd.integers(0, cnt, endpoint=False)
     return 0, 0, (frto[idx, 0], frto[idx, 1])
     
--- local.py
+++ local.py
@@ -1,153 +1,233 @@
 
-from itertools import product
+from collections.abc import Iterator
+from itertools import chain, product, cycle, repeat
 from dataclasses import dataclass, field
 from collections import deque
 from textwrap import dedent
 from time import time
-from fianco import *
-from bot import think, scr_at
+from typing import TypeAlias
+from concurrent.futures import ThreadPoolExecutor, Future
+from pydantic import BaseModel
+from jinja2 import Template
+from getch import pause # type: ignore
+from typing_extensions import Self
 
+import pickle as pkl
+import json
 import numpy as np
 import pygame as pyg
+import fianco as fnc
+import bot # type: ignore
+
+
+Suggestion: TypeAlias = tuple[int, float, float, tuple[fnc.YX, fnc.YX]]
+Auto: TypeAlias = Future[Suggestion]
+
+
+class Settings(BaseModel):
+   ply: int
+
+
+def load_stng() -> Settings:
+    with open('game.json', 'r') as file:
+        return Settings.model_validate(json.load(file))
+
+
+header = Template('========== {{ player }} ==========')
+action = Template('action: {{ action }}')
+result = Template('result: {{ result }}')
+stats = Template(dedent("""nc:     {{ nodes }} (nodes)
+Δt:     {{ delta }} (seconds)
+nc/Δt:  {{ nodes / delta }} (nodes / seconds)
+vl:     {{ value }}"""))
+move = Template('from {{ fr }} to {{ to }}')
 
 
 @dataclass
-class GameState:
-    game: Engine = field(default_factory=lambda: Engine())
+class Game:
+    state: fnc.Engine = field(default_factory=lambda: fnc.Engine())
     run: bool = field(default_factory=lambda: True)
-    vrts: deque[YX] = field(default_factory=deque)
+    frto: deque[fnc.YX] = field(default_factory=deque)
     disp: pyg.Surface = field(default_factory=lambda: pyg.display.set_mode(((wth := 640), wth)))
+    cell: tuple[int, int] = field(default_factory=lambda: (0, 0))
+    auto: Auto | None = None
+    anim: Iterator[str] = field(default_factory=lambda: iter(cycle(chain(*(repeat(c, 7) for c in ['◜', 
+                                                                                                  '◠', 
+                                                                                                  '◝', 
+                                                                                                  '◞', 
+                                                                                                  '◡', 
+                                                                                                  '◟'])))))
+    ply: int = field(default_factory=lambda: load_stng().ply)
+
+    def to_disk(self) -> Self:
+        with open('game.pkl', 'wb') as file:
+            pkl.dump((self.state.wrt, self.state.brd, self.state.hst), file)
+        return self
+
+    def from_disk(self) -> Self:
+        try:
+            with open('game.pkl', 'rb') as file:
+                self.state.wrt, self.state.brd, self.state.hst = pkl.load(file)
+            return self
+        except FileNotFoundError:
+            return self
 
 
-def ply2txt(ply: int) -> str:
-    return 'white' if ply else 'black'
+def get_disp_brd_fac(game: Game) -> tuple[tuple[int, int], tuple[int, int], tuple[int, int]]:
+    disp_wth, disp_hgt = game.disp.get_size()
+    brd_hgt, brd_wth = game.state.brd.shape[1:]
+    fac_y = int(disp_hgt / brd_hgt)
+    fac_x = int(disp_wth / brd_wth)
+    return (disp_hgt, disp_wth), (brd_hgt, brd_wth), (fac_y, fac_x)
 
 
-def mod2txt(*vrts: YX) -> str:
-    substr = ''
-    if len(vrts) >= 1:
-        substr = f'from {vrts[0]}'
-    if len(vrts) >= 2:
-        substr = f"{substr} to {vrts[1]}"
-    return substr
-
-
-def done(gmst: GameState) -> bool:
-    return gmst.game.winner is not None
-
-
-def paint(cur_yx: YX, gmst: GameState) -> tuple[int, int]:
-    gmst.disp.fill((241, 214, 171))
-    wth, hgt = gmst.disp.get_size()
-    dmi, dmj = gmst.game.brd.shape[1:]
-    cly = int(hgt / dmi)
-    clx = int(wth / dmj)
-    # draw board
-    mat = gmst.game.brd[0] + 2 * gmst.game.brd[1]
-    rad = min(clx, cly) / 3
-    for i, j in product(range(dmi), range(dmj)):
-        xy = (j * clx + clx / 2, i * cly + cly / 2)
-        pyg.draw.circle(gmst.disp, (0, 0, 0), xy, 3)
-        if mat[i, j] != 0:
-            pyg.draw.circle(gmst.disp, (0, 0, 0), xy, rad)
-            pyg.draw.circle(gmst.disp, (50, 50, 50) if mat[i, j] == 1 else (230, 230, 230), xy, rad - 0.1 * rad)
-    # draw line
-    caption = ''
-    if len(gmst.vrts) == 1:
-        fr_yx = gmst.vrts[0]
-        fr_y, fr_x = fr_yx.astype(np.int16)
-        to_y, to_x = cur_yx.astype(np.int16)
-        pyg.draw.line(gmst.disp, (0, 0, 0), (fr_x * clx + clx // 2, fr_y * cly + cly // 2), (to_x * clx + clx // 2, to_y * cly + cly // 2), 1)
-        caption = f'{ply2txt(gmst.game.ply)} | {mod2txt(fr_yx, cur_yx)}'
-    elif not done(gmst):
-        caption = f'{ply2txt(gmst.game.ply)} | {mod2txt(cur_yx)}'
-    else:
-        caption = f'Finished! {ply2txt(0 if win(0, gmst.game.end, gmst.game.brd) else 1)} wins!'
-    pyg.display.set_caption(caption)
-    pyg.display.flip()
-    return cly, clx
-
-
-def cursor_at(cly: int, clx: int) -> YX:
+def get_cursor(fac_y: int, fac_x: int) -> fnc.YX:
     x, y = pyg.mouse.get_pos()
-    return np.array((int(y / cly), int(x / clx)), number)
+    return np.array((int(y / fac_y), int(x / fac_x)), fnc.number)
+
+
+def draw_brd(game: Game) -> None:
+    game.disp.fill((241, 214, 171))
+    _, (brd_hgt, brd_wth), (fac_y, fac_x) = get_disp_brd_fac(game)
+    rad = min(fac_y, fac_x) / 3
+    for i, j in product(range(brd_hgt), range(brd_wth)):
+        xy = (fac_x * (j + 0.5), fac_y * (i + 0.5))
+        pyg.draw.circle(game.disp, (0, 0, 0), xy, 3)
+        if np.any(game.state.brd[:, i, j] != 0):
+            pyg.draw.circle(game.disp, (0, 0, 0), xy, rad)
+            color = (50,) * 3
+            if game.state.brd[1, i, j] == 1:
+                color = (230,) * 3
+            pyg.draw.circle(game.disp, color, xy, rad - 0.1 * rad)
+
+
+def draw_line(game: Game) -> None:
+    if len(game.frto) == 1:
+        _, _, (fac_y, fac_x) = get_disp_brd_fac(game)
+        fr_yx = game.frto[0]
+        fr_y, fr_x = fr_yx.astype(np.int16)
+        to_y, to_x = get_cursor(fac_y, fac_x).astype(np.int16)
+        game.cell = (to_y, to_x)
+        pyg.draw.line(game.disp, 
+                      (0, 0, 0), 
+                      (fr_x * fac_x + fac_x // 2, fr_y * fac_y + fac_y // 2), 
+                      (to_x * fac_x + fac_x // 2, to_y * fac_y + fac_y // 2), 
+                      1)
+
+
+def vrts_as_str(game: Game) -> str:
+    _, _, (fac_y, fac_x) = get_disp_brd_fac(game)
+    y, x = get_cursor(fac_y, fac_x).tolist()
+    if len(game.frto) == 0:
+        return f'{y, x}'
+    if len(game.frto) == 1:
+        return f'{game.frto[0]} --> {y, x}'
+    if len(game.frto) == 2:
+        return f'{game.frto[0]} --> {game.frto[1]}'
+    raise NotImplementedError(f'expected 0 <= len(vrts) <= 2 - got {len(game.frto)}')
+
+
+def wrt_as_str(wrt: int) -> str:
+    if wrt == 0:
+        return 'black'
+    if wrt == 1:
+        return 'white'
+    raise NotImplementedError(f'`wrt` should be 0 or 1 - was {wrt}')
+
+
+def update_title(game: Game) -> None:
+    caption = ''
+    if game.state.wins == fnc.End.BLACK_WINS:
+        caption = 'Black wins!'
+    elif game.state.wins == fnc.End.WHITE_WINS:
+        caption = 'White wins!'
+    elif game.state.wins == fnc.End.TIE:
+        caption = 'Tie!'
+    elif game.auto is None:
+        caption = f'[{wrt_as_str(game.state.wrt)}]: {vrts_as_str(game)}'
+    else:
+        caption = f'[{wrt_as_str(game.state.wrt)}]: {next(game.anim)}'
+    pyg.display.set_caption(caption)
+
+
+def draw_game(game: Game) -> None:
+    draw_brd(game)
+    draw_line(game)
+    update_title(game)
+    pyg.display.flip()
+
+
+def suggest(game: Game, dpth: int) -> Suggestion:
+    t0 = time()
+    nc, vl, frto = bot.think(game.state, dpth)
+    t1 = time()
+    return nc, (t1 - t0), vl, frto
 
 
 def main():
     pyg.init()
-    try:
-        gmst = GameState()
-        clock = pyg.time.Clock()
-        cur_yx = np.array((0, 0), number)
-        while gmst.run:
-            assert 0 <= len(gmst.vrts) <= 2, f'yo, why are you moving {len(gmst.vrts)} steps in one turn?!'
-            cur_yx = cursor_at(*paint(cur_yx, gmst))
-            clock.tick(60)
-            for ev in pyg.event.get():
-                if ev.type == pyg.QUIT:
-                    gmst.run = False
-                elif ev.type == pyg.KEYDOWN:
-                    if ev.key == pyg.K_r:
-                        _ply = gmst.game.ply
-                        gmst = GameState()
-                        print(dedent(f"""
-                        ========== P{_ply} ==========
-                        reset!
-                        """).strip())
-                    elif ev.key == pyg.K_z:
-                        kmods = pyg.key.get_mods()
-                        if kmods & pyg.KMOD_CTRL:
-                            _ply = gmst.game.ply
-                            _ok = gmst.game.undo()
-                            print(dedent(f"""
-                            ========== P{_ply} ==========
-                            undo: {_ok}
-                            """).strip())
-                    elif ev.key == pyg.K_x:
-                        kmods = pyg.key.get_mods()
-                        if kmods & pyg.KMOD_CTRL:
-                            _ply = gmst.game.ply
-                            print(dedent(f"""
-                            ========== P{_ply} ==========
-                            thinking...""").strip())
-                            t0 = time()
-                            nc, vl, _frto = think(gmst.game, (d := 7))
-                            t1 = time()
-                            gmst.vrts.extend(_frto)
-                            fr_yx = _frto[0]
-                            to_yx = _frto[1]
-                            print(dedent(f"""
-                            advice:
-                              {int(fr_yx[0]), int(fr_yx[1])} -> {int(to_yx[0]), int(to_yx[1])}
-                              within {d} (from {gmst.game.trn + 1} to {gmst.game.trn + d}) get {vl}
-                            stats:
-                              D = {d}
-                              Δ = {t1 - t0}
-                              N = {nc}
-                              N/Δ = {nc / (t1 - t0 + (ϵ := 1e-42))}
-                            """).strip())
-                elif ev.type == pyg.MOUSEBUTTONDOWN:
-                    if ev.button == pyg.BUTTON_RIGHT:
-                        gmst.vrts.clear()
-                    elif ev.button == pyg.BUTTON_LEFT:
-                        gmst.vrts.append(cur_yx)
-            while len(gmst.vrts) > 2:
-                gmst.vrts.pop()
-            if len(gmst.vrts) == 2:
-                fr_yx, to_yx = gmst.vrts
-                gmst.vrts.clear()
-                _ply = gmst.game.ply
-                _ok = gmst.game.play(fr_yx, to_yx)
-                print(dedent(f"""
-                ========== P{_ply} ==========
-                trn: {gmst.game.trn}
-                mov: {int(fr_yx[0]), int(fr_yx[1])} -> {int(to_yx[0]), int(to_yx[1])}
-                scr: {scr_at(_ply, gmst.game.end, gmst.game.brd)}
-                ok: {_ok}
-                """).strip())
-    finally:
-        pyg.quit()
+    with ThreadPoolExecutor(max_workers=2) as exe:
+        try:
+            game = Game().from_disk()
+            print(f'bots will search {game.ply}-ply')
+            print(header.render(player=wrt_as_str(game.state.wrt)))
+            clock = pyg.time.Clock()
+            while game.run:
+                assert 0 <= len(game.frto) <= 2, f'yo, why are you moving {len(game.frto)} steps in one turn?!'
+                draw_game(game)
+                clock.tick(60)
+                for ev in pyg.event.get():
+                    kmods = pyg.key.get_mods()
+                    if ev.type == pyg.QUIT:
+                        game.run = False
+                    elif ev.type == pyg.KEYDOWN and ev.key == pyg.K_r and kmods & pyg.KMOD_CTRL:
+                        print(action.render(action='reset'))
+                        if game.auto is not None:
+                            game.auto.cancel()
+                        game.auto = None
+                        game = Game()
+                        print(header.render(player=wrt_as_str(game.state.wrt)))
+                    elif ev.type == pyg.KEYDOWN and ev.key == pyg.K_z and kmods & pyg.KMOD_CTRL:
+                        ok = game.state.undo()
+                        if ok:
+                            print(action.render(action='undo'))
+                            if game.auto is not None:
+                                game.auto.cancel()
+                            game.auto = None
+                    elif ev.type == pyg.KEYDOWN and ev.key == pyg.K_x and kmods & pyg.KMOD_CTRL:
+                        if game.auto is None:
+                            game.auto = exe.submit(suggest, game, game.ply)
+                    elif ev.type == pyg.MOUSEBUTTONDOWN:
+                        if ev.button == pyg.BUTTON_RIGHT:
+                            game.frto.clear()
+                        if ev.button == pyg.BUTTON_LEFT:
+                            _, _, (fac_y, fac_x) = get_disp_brd_fac(game)
+                            cursor = get_cursor(fac_y, fac_x)
+                            game.frto.append(cursor)
+                if game.auto is not None and game.auto.done():
+                    if (err := game.auto.exception()) is not None:
+                        print(err)
+                    else:
+                        game.frto.clear()
+                        nc, Δt, vl, frto = game.auto.result()
+                        game.frto.extend(frto)
+                        print(stats.render(nodes=nc, delta=Δt, value=vl))
+                    game.auto = None
+                while len(game.frto) > 2:
+                    game.frto.pop()
+                if len(game.frto) == 2 and game.auto is None:
+                    ok = game.state.play(*game.frto)
+                    if ok:
+                        game.to_disk()
+                        print(action.render(action='move'))
+                        print(move.render(fr=game.frto[0], to=game.frto[1]))
+                        print(header.render(player=wrt_as_str(game.state.wrt)))
+                    game.frto.clear()
+        finally:
+            pyg.quit()
+            exe.shutdown(wait=True, cancel_futures=True)
 
 
 if __name__ == '__main__':
     main()
+    pause()
